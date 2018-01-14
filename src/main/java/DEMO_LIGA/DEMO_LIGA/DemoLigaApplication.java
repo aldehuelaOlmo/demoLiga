108d184cee8cbfbb0de85e000e41988aa31969f6
aldehuelaOlmo <andres-aldehuela123@hotmail.com>
2018-01-14 13:31:46 +0100

    Reinitialized files

---
+++ src/main/java/DEMO_LIGA/DEMO_LIGA/DemoLigaApplication.java
@@ -0,0 +1,12 @@
+package DEMO_LIGA.DEMO_LIGA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLigaApplication.class, args);
+	}
+}
